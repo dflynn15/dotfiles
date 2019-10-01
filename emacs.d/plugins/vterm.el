d8b7c87a28e8a71157cd5f35755421e7f1e49ae7
Daniel Flynn <daniel.flynn@gusto.com>
2019-10-01 11:25:28 -0400

    feat: add zoom to vterm

--- emacs.d/plugins/vterm.el
+++ emacs.d/plugins/vterm.el
@@ -2,6 +2,7 @@
   :load-path "~/Projects/emacs-libvterm"
   :defer 1
   :config (progn
+    (setq vterm-shell "/bin/zsh")
     (global-set-key (kbd "C-SPC v n") 'vterm)
   )
   :ensure nil)
--- emacs.d/plugins/zoom-window.el
+++ emacs.d/plugins/zoom-window.el
@@ -3,6 +3,7 @@
   :diminish ""
   :init (progn
     (global-set-key (kbd "C-SPC z") 'zoom-window-zoom)
+    (evil-leader/set-key "z" 'zoom-window-zoom)
     (custom-set-variables
       '(zoom-window-mode-line-color "base16-atelier-seaside-light"))
   )
